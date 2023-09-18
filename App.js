29bac4e0f133d5f5e5bb557f4f4367c2ce432897
Ummara-hussain <ummarahussain9@gmail.com>
2023-09-18 22:24:22 +0500

    Assign2 done

--- App.js
+++ App.js
@@ -1,39 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-const name = "Hello World";
-const obj = {name: "Hello World Object"};
-const data = ['We', 'are', 'United'];
-const list = [{name: "Hello World 1"}, {name: "Hello World 2"}, {name: "Hello World 3"}];
-const complex = [{company: 'XYZ', jobs: ['Javascript', 'React']}, {company: 'ABC', jobs: ['AngularJs' ,'Ionic']}]
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{name}</h1>
-        <h1>{obj.name}</h1>
-        <div>
-        {data.map(function(item){
-          return <p> {item}</p>
-        })}
-        </div>
-            <ul>
-        {list.map(function(item){
-          return <li>{item.name}</li>
-        })}
-        </ul>
-        <table border='1'>
-          <tr>
-            <th>Company</th>
-            <th>Jobs</th>
-          </tr>
-          {complex.map(function(item){
-            return <tr><td>{item.company}</td> <td>{item.jobs[0]} , {item.jobs[1]}</td></tr>
-          })}
-        </table>
+import {useState} from 'react'
 
-      </header>
+function App() {
+const [list,setList] = useState([])
+const [userInput,setUserInput] = useState([])
+
+function userInputValue(e){
+  setUserInput(e.target.value)
+}
+function addToDo(){
+  const tempList = [...list]
+  tempList.push(userInput)
+  setList(tempList)
+}
+function dltli(index){
+  const tempList = [...list]
+  tempList.splice(index, 1)
+  setList(tempList)
+}
+function editBtn(index){
+const tempList = [...list]
+const abc = prompt('Update your todo')
+tempList.splice(index,1,abc)
+setList(tempList)
+}
+
+    return (
+      <div className="App">
+        <header className="App-header">
+
+            <input onChange={userInputValue} placeholder='Enter your todo'/> <br></br>
+            <button onClick={addToDo}>Add</button>
+            <ul>
+            {list.map(function(item, index){
+            return <li>{item}
+            <button onClick={editBtn}>Edit</button>
+            <button onClick={() => dltli(index)}>Delete</button>
+            </li>
+          }
+            )}
+            </ul>
+
+
+        </header>
     </div>
   );
 }
-
 export default App;
