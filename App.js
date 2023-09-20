f6a0e582609ac9770c58482148b21cc87d4c9760
Ummara-hussain <ummarahussain9@gmail.com>
2023-09-20 22:44:20 +0500

    assign3

--- App.js
+++ App.js
@@ -1,49 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
+import { useState } from 'react'
 
 function App() {
-const [list,setList] = useState([])
-const [userInput,setUserInput] = useState([])
+  const [list, setList] = useState([])
+  const [userInput, setUserInput] = useState([])
+  const [editMode, setEditMode] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState()
 
-function userInputValue(e){
-  setUserInput(e.target.value)
-}
-function addToDo(){
-  const tempList = [...list]
-  tempList.push(userInput)
-  setList(tempList)
-}
-function dltli(index){
-  const tempList = [...list]
-  tempList.splice(index, 1)
-  setList(tempList)
-}
-function editBtn(index){
-const tempList = [...list]
-const abc = prompt('Update your todo')
-tempList.splice(index,1,abc)
-setList(tempList)
-}
+  function userInputValue(e) {
+    setUserInput(e.target.value)
+  }
+  function addToDo() {
+    const tempList = [...list]
+    tempList.push(userInput)
+    setList(tempList)
+    setUserInput('')
+  }
+  function dltli(index) {
+    const tempList = [...list]
+    tempList.splice(index, 1)
+    setList(tempList)
+  }
+  function editBtn(index) {
+    const value = list[index]
+    setUserInput(value)
+    setEditMode(true)
+    setCurrentIndex(index)
+  }
+  function updateBtn() {
+    const tempList = [...list]
+    tempList[currentIndex] = userInput
+    setList(tempList)
+    setEditMode(false)
+    setUserInput('')
+    setCurrentIndex('')
+  }
+  function dltAll() {
+    setList([])
+  }
 
-    return (
-      <div className="App">
-        <header className="App-header">
+  return (
+    <div className="App">
+      <header className="App-header">
 
-            <input onChange={userInputValue} placeholder='Enter your todo'/> <br></br>
-            <button onClick={addToDo}>Add</button>
-            <ul>
-            {list.map(function(item, index){
-            return <li>{item}
-            <button onClick={editBtn}>Edit</button>
-            <button onClick={() => dltli(index)}>Delete</button>
+        <input onChange={userInputValue} placeholder='Enter your todo' value={userInput} /> <br></br>
+        {!editMode ?
+          <button onClick={addToDo}>Add</button>
+          :
+          <button onClick={updateBtn}>Update</button>
+        }
+        <button onClick={dltAll}>Delete All</button>
+        <ul>
+          {list.map(function (item, index) {
+            return <li style={currentIndex === index ? { backgroundColor: 'red' } : {}}>
+              {item}
+              <button onClick={() => editBtn(index)}>Edit</button>
+              <button onClick={() => dltli(index)}>Delete</button>
             </li>
           }
-            )}
-            </ul>
+          )}
+        </ul>
 
 
-        </header>
+      </header>
     </div>
   );
 }
